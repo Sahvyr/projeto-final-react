5b97e08d9a5dda823e62063e971be7ecb896b3e4
Sahvyr <thiago.rodrigues365701@gmail.com>
2025-07-01 20:37:03 -0300

    Adiciona pacote de ícones do MUI e atualiza layout da página inicial
    Integra a dependência de ícones do MUI para dar suporte ao uso de ícones na interface.
    Refina a estrutura da página inicial ao incluir uma barra de navegação personalizada
    e simplifica o CSS global para melhorar a manutenção.

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Button from "@mui/material/Button";
-
+import AppBar from "../organisms/AppBar";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const navigate = useNavigate();
   return (
     <div>
+      <AppBar />
       <h1>Bem-vindo à Home</h1>
       <p>Esta é a página inicial do aplicativo.</p>
       <Button variant="contained" onClick={() => navigate("/dashboard")}>
